54e01874d5bf4c82f4af3b356ba70e8c4b002dd1
alek <wait82985@gmail.com>
2024-05-27 12:47:45 +0500

    add wordScores array

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,31 @@
+import java.util.Arrays;
+
 public class Solution {
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
         int[] letCount = new int[26];
         for(int i = 0; i < letters.length; i++) {
             letCount[letters[i] - 'a']++;
         }
+        int[] wordScores = new int[words.length];
+        for(int i = 0; i < words.length; i++ ) {
+            for (int j = 0; j < words[i].length(); j++) {
+                wordScores[i] += score[words[i].charAt(j) - 'a'];
+            }
+        }
+    }
+    int dfs(int[] letCount, String[] words, int[] score, int i){
 
+    }
+    int canForm(String s, int[] letCount, int[] score ) {
+        int[] copy = Arrays.copyOf(letCount, letCount.length);
+        int res = 0;
+        for(int i = 0; i < s.length(); i++) {
+            copy[s.charAt(i) - 'a']--;
+            if(copy[s.charAt(i) - 'a'] < 0) {
+                return -1;
+            }
+            res += score[s.charAt(i) - 'a'];
+        }
+        return res;
     }
 }
